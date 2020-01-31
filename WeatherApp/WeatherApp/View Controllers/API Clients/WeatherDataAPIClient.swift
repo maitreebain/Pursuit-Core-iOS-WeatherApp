5301ace370df10fcd7d1cf98d6f8f10611b4902f
maitreebain <maitreebain@gmail.com>
2020-01-31 17:17:14 -0500

    working on weatherData API client

---
+++ WeatherApp/WeatherApp/View Controllers/API Clients/WeatherDataAPIClient.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherDataAPIClient.swift
+//  WeatherApp
+//
+//  Created by Maitree Bain on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
+//fndb
